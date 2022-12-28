30e927e8117bd2f0e687f3b4ad1d505c1fcff050
Binay Shaw <binayshaw7777@gmail.com>
2022-12-28 23:30:29 +0530

    Implemented UI for Register Activity.

--- app/src/main/java/com/binay/shaw/justap/ui/authentication/SignUp_Screen.kt
+++ app/src/main/java/com/binay/shaw/justap/ui/authentication/SignUp_Screen.kt
@@ -1,10 +1,13 @@
 package com.binay.shaw.justap.ui.authentication
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.method.HideReturnsTransformationMethod
+import android.text.method.PasswordTransformationMethod
+import android.view.MotionEvent
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatDelegate
-import androidx.appcompat.widget.Toolbar
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.binay.shaw.justap.R
 import com.binay.shaw.justap.databinding.ActivitySignUpScreenBinding
@@ -22,5 +25,42 @@ class SignUp_Screen : AppCompatActivity() {
         supportActionBar?.hide()
         findViewById<TextView>(R.id.toolbar_title).text = "Create Account"
 
+        passwordVisibilityHandler()
+
+    }
+
+    @SuppressLint("ClickableViewAccessibility")
+    private fun passwordVisibilityHandler() {
+        // Hide and Show Password
+        var passwordVisible = false
+        binding.etPassword.setOnTouchListener { _, event ->
+            val right = 2
+            if (event.action == MotionEvent.ACTION_UP) {
+                if (event.rawX >= binding.etPassword.right - binding.etPassword.compoundDrawables[right].bounds.width()
+                ) {
+                    val selection: Int = binding.etPassword.selectionEnd
+                    //Handles Multiple option popups
+                    if (passwordVisible) {
+                        //set drawable image here
+                        binding.etPassword.setCompoundDrawablesRelativeWithIntrinsicBounds(
+                            0, 0, R.drawable.visibility_on, 0)
+                        //for hide password
+                        binding.etPassword.transformationMethod = PasswordTransformationMethod.getInstance()
+                        passwordVisible = false
+                    } else {
+                        //set drawable image here
+                        binding.etPassword.setCompoundDrawablesRelativeWithIntrinsicBounds(
+                            0, 0, R.drawable.visibility_off, 0)
+                        //for show password
+                        binding.etPassword.transformationMethod = HideReturnsTransformationMethod.getInstance()
+                        passwordVisible = true
+                    }
+                    binding.etPassword.isLongClickable = false //Handles Multiple option popups
+                    binding.etPassword.setSelection(selection)
+                    return@setOnTouchListener true
+                }
+            }
+            false
+        }
     }
 }
