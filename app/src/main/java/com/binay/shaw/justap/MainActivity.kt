c3aa646725fa6ec81c0e562087bed5c55ec23c28
Binay Shaw <binayshaw7777@gmail.com>
2022-12-28 13:56:09 +0530

    Added Splash Screen and Dark Mode Control.

--- app/src/main/java/com/binay/shaw/justap/MainActivity.kt
+++ app/src/main/java/com/binay/shaw/justap/MainActivity.kt
@@ -1,11 +1,31 @@
 package com.binay.shaw.justap
 
-import androidx.appcompat.app.AppCompatActivity
+import android.app.UiModeManager
+import android.content.res.Configuration
 import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatDelegate
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import com.binay.shaw.justap.databinding.ActivityMainBinding
+
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        installSplashScreen()
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.UIModeSwitch.setOnCheckedChangeListener { _, b ->
+            if (b)
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+            else
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+        }
+
     }
 }
