1b66f0945f133d9d2512bef8778abe82933d6363
Binay Shaw <binayshaw7777@gmail.com>
2022-12-29 14:22:02 +0530

    Updated UI and implemented SignIn feature.

---
+++ app/src/main/java/com/binay/shaw/justap/ui/authentication/ForgotPassword_Screen.kt
@@ -0,0 +1,12 @@
+package com.binay.shaw.justap.ui.authentication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.binay.shaw.justap.R
+
+class ForgotPassword_Screen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_forgot_password_screen)
+    }
+}
--- app/src/main/java/com/binay/shaw/justap/ui/authentication/SignIn_Screen.kt
+++ app/src/main/java/com/binay/shaw/justap/ui/authentication/SignIn_Screen.kt
@@ -11,10 +11,10 @@ import android.view.MotionEvent
 import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatDelegate
-import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import com.binay.shaw.justap.MainActivity
 import com.binay.shaw.justap.R
 import com.binay.shaw.justap.Util
-import com.binay.shaw.justap.databinding.ActivitySignUpScreenBinding
+import com.binay.shaw.justap.databinding.ActivitySignInScreenBinding
 import com.google.firebase.auth.FirebaseAuth
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
@@ -22,61 +22,66 @@ import kotlinx.coroutines.launch
 import kotlinx.coroutines.tasks.await
 import kotlinx.coroutines.withContext
 
-class SignUp_Screen : AppCompatActivity() {
+class SignIn_Screen : AppCompatActivity() {
 
-    private lateinit var binding: ActivitySignUpScreenBinding
+    private lateinit var binding: ActivitySignInScreenBinding
     private lateinit var auth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        installSplashScreen()
-        binding = ActivitySignUpScreenBinding.inflate(layoutInflater)
+        binding = ActivitySignInScreenBinding.inflate(layoutInflater)
         AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
         setContentView(binding.root)
         supportActionBar?.hide()
         auth = FirebaseAuth.getInstance()
-        findViewById<TextView>(R.id.toolbar_title).text = "Create Account"
+        findViewById<TextView>(R.id.toolbar_title).text = "Log In"
 
         passwordVisibilityHandler()
 
-        binding.btnCreateAccount.setOnClickListener {
-            createNewAccount()
+        binding.btnLogIn.setOnClickListener {
+            loginUser()
         }
 
-        binding.loginInstead.setOnClickListener {
-            startActivity(Intent(this@SignUp_Screen, SignIn_Screen::class.java))
+        binding.createAccount.setOnClickListener {
+            onBackPressedDispatcher.onBackPressed()
         }
 
+        binding.forgotPassword.setOnClickListener {
+            startActivity(Intent(this@SignIn_Screen, ForgotPassword_Screen::class.java))
+        }
     }
 
-    private fun createNewAccount() {
-        val userName = binding.etName.text.toString()
+
+    private fun loginUser() {
         val userEmail = binding.etEmail.text.toString()
         val userPassword = binding.etPassword.text.toString()
-
-        if (userName.isNotEmpty() && userEmail.isNotEmpty() && userPassword.isNotEmpty()
-            && Patterns.EMAIL_ADDRESS.matcher(userEmail).matches()) {
-            if (userPassword.length > 7) {
-                CoroutineScope(Dispatchers.IO).launch {
-                    try {
-                        auth.createUserWithEmailAndPassword(userEmail, userPassword).addOnCompleteListener {
-                            if (it.isSuccessful) {
-                                Toast.makeText(this@SignUp_Screen, "Successfully Registered", Toast.LENGTH_LONG).show()
-                                startActivity(Intent(this@SignUp_Screen, SignIn_Screen::class.java))
-                            }
-                        }.await()
-                        withContext(Dispatchers.Main) {
-                            checkLoggedInState()
-                        }
-                    } catch (e: Exception) {
-                        withContext(Dispatchers.Main) {
-                            Toast.makeText(this@SignUp_Screen, e.message, Toast.LENGTH_LONG).show()
-                        }
+        if (userEmail.isNotEmpty() && userPassword.isNotEmpty()
+            && Patterns.EMAIL_ADDRESS.matcher(userEmail).matches() && userPassword.length > 7
+        ) {
+            CoroutineScope(Dispatchers.IO).launch {
+                try {
+                    auth.signInWithEmailAndPassword(userEmail, userPassword).addOnSuccessListener {
+                        Toast.makeText(
+                            this@SignIn_Screen,
+                            "Successfully Logged In",
+                            Toast.LENGTH_LONG
+                        ).show()
+                        startActivity(Intent(this@SignIn_Screen, MainActivity::class.java))
+                            .also { finish() }
+                    }.await()
+                    withContext(Dispatchers.Main) {
+                        checkLoggedInState()
+                    }
+                } catch (e: Exception) {
+                    withContext(Dispatchers.Main) {
+                        Toast.makeText(this@SignIn_Screen, e.message, Toast.LENGTH_LONG).show()
                     }
                 }
             }
-        }
-
+        } else if (userEmail.isEmpty() || Patterns.EMAIL_ADDRESS.matcher(userEmail).matches())
+            Toast.makeText(this@SignIn_Screen, "Check your email", Toast.LENGTH_LONG).show()
+        else if (userPassword.isEmpty() || userPassword.length < 8)
+            Toast.makeText(this@SignIn_Screen, "Check your password", Toast.LENGTH_LONG).show()
     }
 
     private fun checkLoggedInState() {
@@ -86,11 +91,6 @@ class SignUp_Screen : AppCompatActivity() {
         } else {
             Util.log("You are logged in!")
         }
-    }
-
-    override fun onStart() {
-        super.onStart()
-        checkLoggedInState()
     }
 
     @SuppressLint("ClickableViewAccessibility")
@@ -107,16 +107,20 @@ class SignUp_Screen : AppCompatActivity() {
                     if (passwordVisible) {
                         //set drawable image here
                         binding.etPassword.setCompoundDrawablesRelativeWithIntrinsicBounds(
-                            0, 0, R.drawable.visibility_on, 0)
+                            0, 0, R.drawable.visibility_on, 0
+                        )
                         //for hide password
-                        binding.etPassword.transformationMethod = PasswordTransformationMethod.getInstance()
+                        binding.etPassword.transformationMethod =
+                            PasswordTransformationMethod.getInstance()
                         passwordVisible = false
                     } else {
                         //set drawable image here
                         binding.etPassword.setCompoundDrawablesRelativeWithIntrinsicBounds(
-                            0, 0, R.drawable.visibility_off, 0)
+                            0, 0, R.drawable.visibility_off, 0
+                        )
                         //for show password
-                        binding.etPassword.transformationMethod = HideReturnsTransformationMethod.getInstance()
+                        binding.etPassword.transformationMethod =
+                            HideReturnsTransformationMethod.getInstance()
                         passwordVisible = true
                     }
                     binding.etPassword.isLongClickable = false //Handles Multiple option popups
