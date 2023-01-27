b4fbf8811f1481b2fcb626ba96a8f8b3a988bee4
Binay Shaw <binayshaw7777@gmail.com>
2023-01-27 14:28:59 +0530

    Added layout of Add account fragment.

---
+++ app/src/main/java/com/binay/shaw/justap/model/Accounts.kt
@@ -0,0 +1,11 @@
+package com.binay.shaw.justap.model
+
+/**
+ * Created by binay on 27,January,2023
+ */
+
+data class Accounts(
+    val accountID: Int,
+    val accountName: String,
+    val accountData: String
+)
--- app/src/main/java/com/binay/shaw/justap/ui/mainScreens/accountFragments/AddEditFragment.kt
+++ app/src/main/java/com/binay/shaw/justap/ui/mainScreens/accountFragments/AddEditFragment.kt
@@ -5,6 +5,9 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import com.binay.shaw.justap.MainActivity
 import com.binay.shaw.justap.R
 import com.binay.shaw.justap.databinding.FragmentAddEditBinding
 
@@ -12,6 +15,10 @@ class AddEditFragment : Fragment() {
 
     private var _binding: FragmentAddEditBinding? = null
     private val binding get() = _binding!!
+    private lateinit var toolbarText: TextView
+    private lateinit var toolbarBackButton: ImageView
+    private lateinit var toolbarDeleteIcon: ImageView
+    private val mode: Int = 0
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -29,6 +36,16 @@ class AddEditFragment : Fragment() {
 
     private fun initialization(container: ViewGroup?) {
         _binding = FragmentAddEditBinding.inflate(layoutInflater, container, false)
-
+        (activity as MainActivity).supportActionBar?.hide()
+        toolbarText = binding.root.findViewById(R.id.toolbar_title)
+        if (mode == 0)
+            toolbarText.text = "Add account"
+        else {
+            toolbarText.text = "Edit account"
+            toolbarDeleteIcon = binding.root.findViewById(R.id.rightIcon)
+            toolbarDeleteIcon.visibility = View.VISIBLE
+        }
+        toolbarBackButton = binding.root.findViewById(R.id.leftIcon)
+        toolbarBackButton.visibility = View.VISIBLE
     }
 }
