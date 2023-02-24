e0db90a350be0b19dc82017d882849f7a3a3e307
Binay Shaw <binayshaw7777@gmail.com>
2023-02-24 15:09:43 +0530

    Optimized SignIn Screen 1

---
+++ app/src/main/java/com/binay/shaw/justap/Constants.kt
@@ -0,0 +1,18 @@
+package com.binay.shaw.justap
+
+
+object Constants {
+    val localDB = "localDB"
+    val users = "Users"
+    val userID = "userID"
+    val name = "name"
+    val email = "email"
+    val profilePictureUri = "profilePictureURI"
+    val profileBannerUri = "profileBannerURI"
+    val bio = "bio"
+    val accounts = "accounts"
+    val accountId = "accountID"
+    val accountName = "accountName"
+    val accountData = "accountData"
+    val showAccount = "showAccount"
+}
--- app/src/main/java/com/binay/shaw/justap/ui/authentication/signInScreen/SignInViewModel.kt
+++ app/src/main/java/com/binay/shaw/justap/ui/authentication/signInScreen/SignInViewModel.kt
@@ -3,37 +3,25 @@ package com.binay.shaw.justap.ui.authentication.signInScreen
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.binay.shaw.justap.Constants
 import com.binay.shaw.justap.helper.Util
 import com.binay.shaw.justap.model.Accounts
 import com.binay.shaw.justap.model.User
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseReference
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.tasks.await
 import kotlinx.coroutines.withContext
 
-/**
- * Created by binay on 31,December,2022
- */
+
 class SignInViewModel : ViewModel() {
     var status = MutableLiveData<Int>()
     private val auth: FirebaseAuth = FirebaseAuth.getInstance()
     private val errorMessage = MutableLiveData<String>()
     var firebaseUser = MutableLiveData<User>()
     var firebaseAccounts = MutableLiveData<List<Accounts>>()
-
-    /** Status values
-     * 0 - Default
-     * 1 - name is empty
-     * 2 - email is empty
-     * 3 - email is not valid
-     * 4 - password is empty
-     * 5 - password is less than 8 characters
-     * 6 - Use upper & lowercase with digit & symbols
-     * 7 - success
-     * 8 - Fail
-     * */
 
     init {
         status.value = 0
@@ -45,72 +33,29 @@ class SignInViewModel : ViewModel() {
 
     fun loginUser(userEmail: String, userPassword: String, firebaseDatabase: DatabaseReference) {
 
-        when (Util.validateUserAuthInput(null, userEmail, userPassword)) {
-            2 -> {
-                //email is empty
-                status.value = 2
-            }
-            3 -> {
-                //email is not valid
-                status.value = 3
-            }
-            4 -> {
-                //password is empty
-                status.value = 4
-            }
-            5 -> {
-                //password is less than 8 characters
-                status.value = 5
-            }
-            6 -> {
-                //password must contains Uppercase, lowercase and symbols
-                status.value = 6
-            }
+        when (val validate = Util.validateUserAuthInput(null, userEmail, userPassword)) {
             7 -> {
                 //Success
-
                 viewModelScope.launch(Dispatchers.IO) {
                     try {
                         auth.signInWithEmailAndPassword(userEmail, userPassword).await()
-                        if (checkLoggedInState()) {
+                        if (Util.isUserLoggedIn()) {
                             val userID = FirebaseAuth.getInstance().uid.toString()
-                            firebaseDatabase.child("Users").child(userID).get()
+                            firebaseDatabase.child(Constants.users).child(userID).get()
                                 .addOnSuccessListener {
-                                    val id = it.child("userID").value.toString()
-                                    val name = it.child("name").value.toString()
-                                    val email = it.child("email").value.toString()
-                                    val profilePicture = it.child("profilePictureURI").value.toString()
-                                    val profileBanner = it.child("profileBannerURI").value.toString()
-                                    val bio = it.child("bio").value.toString()
 
-                                    if (it.hasChild("accounts")) {
-                                        val accountList = mutableListOf<Accounts>()
+                                    firebaseUser.value = createUserObject(it)
 
-                                        it.child("accounts").children.forEach { iterator ->
-                                            val tempMap = iterator.value as java.util.HashMap<*, *>
-                                            val acc = Accounts(
-                                                (tempMap["accountID"] as Long).toInt(),
-                                                tempMap["accountName"] as String,
-                                                tempMap["accountData"] as String,
-                                                tempMap["showAccount"] as Boolean
-                                            )
-                                            accountList.add(acc)
-                                        }
-
-                                        firebaseAccounts.value = accountList
+                                    if (it.hasChild(Constants.accounts)) {
+                                        firebaseAccounts.value = createAccountsList(it.child(Constants.accounts))
                                     }
 
-                                    firebaseUser.value = User(
-                                        id, name,
-                                        email, bio, profilePicture, profileBanner
-                                    )
-
                                     Util.log(it.value.toString())
-
                                     status.value = 7    //Success
+
                                 }.addOnFailureListener {
-                                    Util.log("Failed to fetch user data")
                                     status.value = 8    //Failed
+                                    errorMessage.value = it.message.toString()
                                 }
                         }
                     } catch (e: Exception) {
@@ -121,17 +66,38 @@ class SignInViewModel : ViewModel() {
                     }
                 }
             }
+            else -> {
+                if (validate < 7)
+                    status.value = validate
+            }
         }
     }
 
-    private fun checkLoggedInState(): Boolean {
-        // not logged in
-        return if (auth.currentUser == null) {
-            Util.log("You are not logged in")
-            false
-        } else {
-            Util.log("You are logged in!")
-            true
+    private fun createAccountsList(child: DataSnapshot): MutableList<Accounts> {
+        val accountList = mutableListOf<Accounts>()
+        child.children.forEach { iterator ->
+            val tempMap = iterator.value as java.util.HashMap<*, *>
+            val acc = Accounts(
+                (tempMap[Constants.accountId] as Long).toInt(),
+                tempMap[Constants.accountName] as String,
+                tempMap[Constants.accountData] as String,
+                tempMap[Constants.showAccount] as Boolean
+            )
+            accountList.add(acc)
+        }
+        return accountList
+    }
+
+    private fun createUserObject(snapshot: DataSnapshot): User {
+        snapshot.apply {
+            val id = child(Constants.userID).value.toString()
+            val name = child(Constants.name).value.toString()
+            val email = child(Constants.email).value.toString()
+            val profilePicture = child(Constants.profilePictureUri).value.toString()
+            val profileBanner = child(Constants.profileBannerUri).value.toString()
+            val bio = child(Constants.bio).value.toString()
+
+            return User(id, name, email, bio, profilePicture, profileBanner)
         }
     }
 }
