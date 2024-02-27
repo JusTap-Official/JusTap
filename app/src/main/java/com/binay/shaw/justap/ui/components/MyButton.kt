5da818543375018de16fea1dbad14f3a520be4d5
binay shaw <binayshaw7777@gmail.com>
2024-02-28 01:28:19 +0530

    DI + Themes + Fonts

---
+++ app/src/main/java/com/binay/shaw/justap/ui/components/MyButton.kt
@@ -0,0 +1,44 @@
+package com.binay.shaw.justap.ui.components
+
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.RowScope
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonColors
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.ButtonElevation
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Shape
+
+@Composable
+fun MyButton(
+    modifier: Modifier = Modifier,
+    text: String,
+    enabled: Boolean = true,
+    shape: Shape = ButtonDefaults.shape,
+    colors: ButtonColors = ButtonDefaults.buttonColors(),
+    elevation: ButtonElevation? = ButtonDefaults.buttonElevation(),
+    border: BorderStroke? = null,
+    contentPadding: PaddingValues = ButtonDefaults.ContentPadding,
+    interactionSource: MutableInteractionSource = remember { MutableInteractionSource() },
+) {
+    Button(
+        onClick = {
+
+    },
+        modifier = modifier,
+        enabled = enabled,
+        shape = shape,
+        colors = colors,
+        elevation = elevation,
+        border = border,
+        contentPadding = contentPadding,
+        interactionSource = interactionSource
+    ) {
+        Text(text = text)
+    }
+}
---
+++ app/src/main/java/com/binay/shaw/justap/ui/themes/Color.kt
@@ -0,0 +1,42 @@
+package com.binay.shaw.justap.ui.themes
+
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+
+
+val PrimaryDark: Color = Color(0xFFA2C9FF)
+//val OnPrimaryDark: Color = Color(0xFF1B72C0)
+val PrimaryContainerDark: Color = Color(0xFF2F4156)
+val OnPrimaryContainerDark: Color = Color(0xFFD7E2FF)
+//val InversePrimaryDark: Color = // Dark variant of inversePrimary color
+//val SecondaryDark: Color = // Dark variant of secondary color
+//val OnSecondaryDark: Color = // Dark variant of onSecondary color
+//val SecondaryContainerDark: Color = // Dark variant of secondaryContainer color
+//val OnSecondaryContainerDark: Color = // Dark variant of onSecondaryContainer color
+//val TertiaryDark: Color = // Dark variant of tertiary color
+//val OnTertiaryDark: Color = // Dark variant of onTertiary color
+//val TertiaryContainerDark: Color = // Dark variant of tertiaryContainer color
+//val OnTertiaryContainerDark: Color = // Dark variant of onTertiaryContainer color
+val BackgroundDark: Color = Color(0xFF0E181E)
+//val OnBackgroundDark: Color = // Dark variant of onBackground color
+//val SurfaceDark: Color = // Dark variant of surface color
+//val OnSurfaceDark: Color = // Dark variant of onSurface color
+//val SurfaceVariantDark: Color = // Dark variant of surfaceVariant color
+//val OnSurfaceVariantDark: Color = // Dark variant of onSurfaceVariant color
+//val SurfaceTintDark: Color = // Dark variant of surfaceTint color
+//val InverseSurfaceDark: Color = // Dark variant of inverseSurface color
+//val InverseOnSurfaceDark: Color = // Dark variant of inverseOnSurface color
+//val ErrorDark: Color = // Dark variant of error color
+//val OnErrorDark: Color = // Dark variant of onError color
+//val ErrorContainerDark: Color = // Dark variant of errorContainer color
+//val OnErrorContainerDark: Color = // Dark variant of onErrorContainer color
+//val OutlineDark: Color = // Dark variant of outline color
+//val OutlineVariantDark: Color = // Dark variant of outlineVariant color
+//val ScrimDark: Color = // Dark variant of scrim color
+//val SurfaceBrightDark: Color = // Dark variant of surfaceBright color
+//val SurfaceContainerDark: Color = // Dark variant of surfaceContainer color
+//val SurfaceContainerHighDark: Color = // Dark variant of surfaceContainerHigh color
+//val SurfaceContainerHighestDark: Color = // Dark variant of surfaceContainerHighest color
+//val SurfaceContainerLowDark: Color = // Dark variant of surfaceContainerLow color
+//val SurfaceContainerLowestDark: Color = // Dark variant of surfaceContainerLowest color
+//val SurfaceDimDark: Color = // Dark variant of surfaceDim color
