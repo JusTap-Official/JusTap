faab2e1b27f4d57501880bcc4bd298336b2de1e0
binay shaw <binayshaw7777@gmail.com>
2024-02-27 21:27:47 +0530

    branch creation + compose dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -25,6 +25,7 @@ android {
 
     buildFeatures {
         viewBinding = true
+        compose = true
     }
 
     buildTypes {
@@ -57,6 +58,30 @@ dependencies {
     val navVersion = "2.5.3"
     val roomVersion = "2.5.1"
     val lottieVersion = "5.2.0"
+
+    // Compose
+    val composeBom = platform("androidx.compose:compose-bom:2023.10.01")
+    implementation(composeBom)
+    implementation("androidx.compose.runtime:runtime")
+    implementation("androidx.compose.ui:ui")
+    implementation("androidx.compose.foundation:foundation")
+    implementation("androidx.compose.foundation:foundation-layout")
+    implementation("androidx.compose.material:material")
+    implementation("androidx.compose.material3:material3:1.2.0")
+    implementation("androidx.compose.runtime:runtime-livedata")
+    implementation("androidx.compose.ui:ui-tooling")
+    implementation("androidx.compose.ui:ui-graphics")
+    implementation("androidx.compose.ui:ui-tooling-preview")
+    implementation("androidx.constraintlayout:constraintlayout-compose:1.0.1")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.7.0")
+    implementation("androidx.activity:activity-compose:1.9.0-alpha03")
+    implementation("androidx.navigation:navigation-compose:2.7.7")
+
+
+    // Compose Test
+    androidTestImplementation(composeBom)
+    androidTestImplementation("androidx.compose.ui:ui-test-junit4")
+    debugImplementation("androidx.compose.ui:ui-test-manifest")
 
     //Default Pre-defined
     implementation ("androidx.core:core-ktx:1.9.0")
