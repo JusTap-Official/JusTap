e12e0a6fd1236c821e188ba0f35427fd719f5fc1
binay shaw <binayshaw7777@gmail.com>
2024-05-19 20:45:34 +0530

    Rewrote signin screen in compose

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -122,6 +122,7 @@ dependencies {
     implementation("androidx.activity:activity-compose:1.9.0-alpha03")
     implementation("androidx.navigation:navigation-compose:2.7.7")
     implementation("androidx.lifecycle:lifecycle-runtime-compose:$lifecycleVersion")
+    implementation("androidx.compose.material:material-icons-extended:1.6.7")
 
 
     // Compose Test
--- app/src/main/java/com/binay/shaw/justap/presentation/authentication/signInScreen/SignInScreen.kt
+++ app/src/main/java/com/binay/shaw/justap/presentation/authentication/signInScreen/SignInScreen.kt
@@ -3,192 +3,203 @@ package com.binay.shaw.justap.presentation.authentication.signInScreen
 
 import android.content.Intent
 import android.os.Bundle
-import android.text.method.PasswordTransformationMethod
-import android.view.View
-import android.widget.Toast
-import androidx.activity.viewModels
-import com.binay.shaw.justap.R
-import com.binay.shaw.justap.base.BaseActivity
-import com.binay.shaw.justap.base.ViewModelFactory
-import com.binay.shaw.justap.databinding.ActivitySignInScreenBinding
-import com.binay.shaw.justap.utilities.Util
-import com.binay.shaw.justap.model.Accounts
-import com.binay.shaw.justap.presentation.authentication.ForgotPasswordScreen
-import com.binay.shaw.justap.presentation.authentication.signUpScreen.SignUpScreen
+import androidx.activity.ComponentActivity
+import androidx.activity.SystemBarStyle
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Key
+import androidx.compose.material.icons.filled.MailOutline
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.derivedStateOf
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.toArgb
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.core.view.WindowCompat
+import androidx.hilt.navigation.compose.hiltViewModel
 import com.binay.shaw.justap.presentation.MainActivity
+import com.binay.shaw.justap.presentation.authentication.ForgotPasswordScreen
+import com.binay.shaw.justap.presentation.themes.DMSansFontFamily
+import com.binay.shaw.justap.presentation.themes.JusTapTheme
+import com.binay.shaw.justap.utilities.Util.findActivity
+import com.binay.shaw.justap.utilities.Validator.Companion.isValidEmail
+import com.binay.shaw.justap.utilities.Validator.Companion.isValidPassword
+import com.binay.shaw.justap.utilities.onClick
 import com.binay.shaw.justap.viewModel.AccountsViewModel
 import com.binay.shaw.justap.viewModel.FirebaseViewModel
 import com.binay.shaw.justap.viewModel.LocalUserViewModel
+import dagger.hilt.android.AndroidEntryPoint
 
-
-class SignInScreen : BaseActivity() {
-
-    private lateinit var binding: ActivitySignInScreenBinding
-    private val accountsViewModel by viewModels<AccountsViewModel> { ViewModelFactory() }
-    private val localUserViewModel by viewModels<LocalUserViewModel> { ViewModelFactory() }
-    private val firebaseViewModel by viewModels<FirebaseViewModel> { ViewModelFactory() }
-    private var isPasswordVisible = false
-
-
+@AndroidEntryPoint
+class SignInScreen : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
-        setTheme()
-        loadLocate()
-        super.onCreate(savedInstanceState)
-        binding = ActivitySignInScreenBinding.inflate(layoutInflater)
-        setContentView(binding.root)
-
-        initObservers()
-        initViews()
-        clickHandlers()
-    }
-
-    private fun clickHandlers() {
-        binding.run {
-
-            //Login Button onClick
-            btnLogIn.progressButtonBg.setOnClickListener {
-                login()
-            }
-
-            createAccount.setOnClickListener {
-                startActivity(Intent(this@SignInScreen, SignUpScreen::class.java))
-            }
-
-            forgotPassword.setOnClickListener {
-                startActivity(Intent(this@SignInScreen, ForgotPasswordScreen::class.java))
-            }
-
-            passwordToggle.setOnClickListener {
-                togglePassword()
-            }
-
-        }
-    }
-
-    private fun login() {
-        if (!Util.checkForInternet(this@SignInScreen)) {
-            Util.showNoInternet(this@SignInScreen)
-            return
-        }
-        showProgress()
-
-        val email = binding.etEmail.text.toString().lowercase().trim()
-        val password = binding.etPassword.text.toString().trim()
-
-        val validation = Util.validateUserAuthInput(null, email, password)
-        if (validation < 7) {
-            stopProgress()
-            handleErrorInput(validation)
-            return
-        }
-
-        firebaseViewModel.logInUser(
-            email, password
+        enableEdgeToEdge(
+            statusBarStyle = SystemBarStyle.light(
+                Color.Transparent.toArgb(), Color.Transparent.toArgb()
+            ),
+            navigationBarStyle = SystemBarStyle.light(
+                Color.Transparent.toArgb(), Color.Transparent.toArgb()
+            )
         )
-    }
+        super.onCreate(savedInstanceState)
 
-    private fun togglePassword() {
-        hideKeyboard()
-        val showPasswordResId =
-            if (isPasswordVisible) R.drawable.visibility_on else R.drawable.visibility_off
-        isPasswordVisible = isPasswordVisible.not()
-        val passwordTransMethod = if (isPasswordVisible) null else PasswordTransformationMethod()
+        WindowCompat.setDecorFitsSystemWindows(window, false)
 
-        binding.passwordToggle.setImageResource(showPasswordResId)
-        binding.etPassword.transformationMethod = passwordTransMethod
-    }
-
-    private fun handleErrorInput(validationCode: Int) {
-        binding.apply {
-            when (validationCode) {
-                2 -> {
-                    emailHelperTV.text = getString(R.string.enter_email)
-                    emailHelperTV.visibility = View.VISIBLE
-                }
-                3 -> {
-                    emailHelperTV.text = getString(R.string.invalid_email)
-                    emailHelperTV.visibility = View.VISIBLE
-                }
-                4 -> {
-                    passwordHelperTV.text = getString(R.string.enter_password)
-                    passwordHelperTV.visibility = View.VISIBLE
-                }
-                5 -> {
-                    passwordHelperTV.text = getString(R.string.small_size_password)
-                    passwordHelperTV.visibility = View.VISIBLE
-                }
-                6 -> {
-                    passwordHelperTV.text = getString(R.string.invalid_password)
-                    passwordHelperTV.visibility = View.VISIBLE
-                }
+        setContent {
+            JusTapTheme {
+                SignInScreenContent()
             }
         }
     }
+}
 
+@Composable
+fun SignInScreenContent(
+    modifier: Modifier = Modifier,
+    accountsViewModel: AccountsViewModel = hiltViewModel(),
+    firebaseViewModel: FirebaseViewModel = hiltViewModel(),
+    localUserViewModel: LocalUserViewModel = hiltViewModel()
+) {
+    val context = LocalContext.current
 
-    private fun initObservers() {
-        firebaseViewModel.run {
-            userLiveData.observe(this@SignInScreen) {
-                fetchUserForLogIn()
-            }
-            errorLiveData.observe(this@SignInScreen) {
-                stopProgress()
-                binding.etPassword.setText("")
-                Toast.makeText(
-                    this@SignInScreen,
-                    "Sign Failed: $it",
-                    Toast.LENGTH_SHORT
-                ).show()
-            }
+    var userEmail by rememberSaveable { mutableStateOf("") }
+    var userPassword by rememberSaveable { mutableStateOf("") }
+    val enableLoginButton by remember {
+        derivedStateOf {
+            userEmail.isValidEmail() && userPassword.isValidPassword()
         }
     }
+    val userLiveData by firebaseViewModel.userLiveData.observeAsState()
 
-    private fun fetchUserForLogIn() {
-        val user = firebaseViewModel.userLiveData.value
-        val listAccounts = firebaseViewModel.accountsLiveData.value
+    LaunchedEffect(userLiveData) {
 
-        binding.progressAnimation.progressParent.visibility = View.GONE
-
-        if (user != null) {
+        userLiveData?.let { user ->
+            val listAccounts = firebaseViewModel.accountsLiveData.value
             localUserViewModel.insertUser(user)
-            saveAccountsList(listAccounts)
-        }
 
-        val intent = Intent(this@SignInScreen, MainActivity::class.java)
-        startActivity(intent)
-        finish()
-    }
+            listAccounts?.let { accounts ->
+                accounts.forEach { account ->
+                    accountsViewModel.insertAccount(account)
+                }
+            }
 
-    private fun saveAccountsList(listAccounts: List<Accounts>?) {
-        if (listAccounts != null) {
-            for (singleAccount in listAccounts) {
-                accountsViewModel.insertAccount(singleAccount)
+            val intent = Intent(context, MainActivity::class.java)
+            context.findActivity()?.let {
+                it.startActivity(intent)
+                it.finish()
             }
         }
     }
 
-    private fun showProgress() {
-        binding.apply {
-            btnLogIn.apply {
-                buttonText.visibility = View.GONE
-                buttonProgress.visibility = View.VISIBLE
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(MaterialTheme.colorScheme.background)
+            .padding(horizontal = 16.dp, vertical = 20.dp)
+            .then(modifier)
+    ) {
+
+        Column {
+
+            Spacer(Modifier.height(10.dp))
+
+            Text(
+                text = "Sign In", style = TextStyle(
+                    fontFamily = DMSansFontFamily,
+                    fontWeight = FontWeight.Medium,
+                    fontSize = 48.sp
+                )
+            )
+
+            Spacer(Modifier.height(40.dp))
+
+            Row(
+                modifier = Modifier.fillMaxWidth()
+            ) {
+                Icon(
+                    imageVector = Icons.Default.MailOutline,
+                    contentDescription = "Mail outline icon"
+                )
+                OutlinedTextField(
+                    value = userEmail,
+                    onValueChange = { userEmail = it },
+                    modifier = Modifier.fillMaxWidth(),
+                    shape = MaterialTheme.shapes.large,
+                    placeholder = { Text(text = "johndoe123@gmail.com") }
+                )
             }
-            emailHelperTV.visibility = View.GONE
-            passwordHelperTV.visibility = View.GONE
-        }
-    }
 
-    private fun stopProgress() {
-        binding.btnLogIn.apply {
-            buttonText.visibility = View.VISIBLE
-            buttonProgress.visibility = View.GONE
-        }
-    }
+            Spacer(Modifier.height(20.dp))
 
-    private fun initViews() {
-        binding.apply {
-            include.toolbarTitle.text = getString(R.string.LogIn)
-            btnLogIn.buttonText.text = getString(R.string.signin)
+            Row {
+                Icon(
+                    imageVector = Icons.Default.Key,
+                    contentDescription = "Key outline icon"
+                )
+                OutlinedTextField(
+                    value = userPassword,
+                    onValueChange = { userPassword = it },
+                    modifier = Modifier.fillMaxWidth(),
+                    shape = MaterialTheme.shapes.large,
+                    placeholder = { Text(text = "johndoe123@gmail.com") }
+                )
+            }
+
+            Spacer(Modifier.height(12.dp))
+
+            Text("Forgot Password?", modifier = Modifier
+                .align(Alignment.End)
+                .onClick {
+                    context.findActivity()?.let {
+                            it.startActivity(Intent(it, ForgotPasswordScreen::class.java))
+                        }
+                })
+
+            Spacer(Modifier.weight(1f))
+
+            Button(
+                onClick = {
+                    firebaseViewModel.logInUser(
+                        userEmail, userPassword
+                    )
+                },
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .align(Alignment.End),
+                colors = ButtonDefaults.buttonColors(containerColor = MaterialTheme.colorScheme.primaryContainer),
+                enabled = enableLoginButton
+            ) {
+                Text("Login", color = MaterialTheme.colorScheme.onPrimaryContainer)
+            }
         }
+
     }
 }
