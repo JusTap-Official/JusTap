8e0baef3fc03269975df381a735522172d02881a
Binay Shaw <binayshaw7777@gmail.com>
2025-01-03 09:15:20 +0530

    di + new google services json

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,5 +10,6 @@ plugins {
     id ("com.android.application") version "7.4.0" apply false
     id ("com.android.library") version "7.4.0" apply false
     id ("org.jetbrains.kotlin.android") version "1.8.10" apply false
+    id ("com.google.dagger.hilt.android") version "2.48" apply false
     id ("com.google.firebase.firebase-perf") version "1.4.2" apply false
 }
